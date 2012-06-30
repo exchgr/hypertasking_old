cf2d4c522a5de83a137742df8da818f75b23d45e
Dan Mundy <drm.mundy@gmail.com>
2012-06-30 11:08:36 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hypertasking::Application.config.secret_token = '73cb7962693812eac5c6732960cd33b1b6e439499842f95c20db5d4ceff75b62c889aef45cab3cd1a9041be02b141392d13903eea2fe11878bd157bc6e8e1c5f'
