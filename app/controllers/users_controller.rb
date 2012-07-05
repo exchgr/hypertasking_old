8b8a2b78d9e3331bd0a2ce732ae188047f0ad781
Dan Mundy <drm.mundy@gmail.com>
2012-07-05 14:38:20 -0400

    Responsive goodness

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      #what to do?
+    else
+      render 'new'
+    end
+  end
+
   def new
     @user = User.new
   end
