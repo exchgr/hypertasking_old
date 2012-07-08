6dfff957594c68e73f8f02b0ba95fdd33805c7fc
Dan Mundy <drm.mundy@gmail.com>
2012-07-08 15:41:27 -0400

    Renames variable in form_errors.js.erb. Adds form error logic to the sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email params[:session][:email]
+    respond_to do |format|
+      if !user
+        format.js {
+          render template: 'shared/ujs/form_errors.js.erb',
+          locals: {
+            ids: ['email'],
+            full_messages: ['No user with that email exists.']
+          }
+        }
+      elsif !user.authenticate params[:session][:password]
+        format.js {
+          render template: 'shared/ujs/form_errors.js.erb',
+          locals: {
+            ids: ['email'],
+            full_messages: ['Wrong password.']
+          }
+        }
+      end
+    end
   end
 
   def destroy
