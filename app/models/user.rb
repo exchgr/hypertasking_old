73c571a9ef13e9784841012a8692068047aab663
Dan Mundy <drm.mundy@gmail.com>
2012-07-08 16:33:04 -0400

    Adds remember_token generator to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save {|user| user.email = email.downcase}
+  before_save :create_remember_token
   
   validates :first_name,
     presence: true, 
@@ -24,4 +25,10 @@ class User < ActiveRecord::Base
 
   validates :password_confirmation,
     presence: true
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
