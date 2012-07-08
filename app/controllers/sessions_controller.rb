dfa2ef2fdd2a7c3985129e858e623b8eb9193a5b
Dan Mundy <drm.mundy@gmail.com>
2012-07-08 15:51:54 -0400

    Shuffles the sessions controller form error logic around to make it more semantically correct in Ruby. Fixes issue where a wrong password error would have displayed under the email field.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,24 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email params[:session][:email]
     respond_to do |format|
-      if !user
+      if user
+        if user.authenticate params[:session][:password]
+          # Sign in and do all the right things.
+        else
+          format.js {
+            render template: 'shared/ujs/form_errors.js.erb',
+            locals: {
+              ids: ['password'],
+              full_messages: ['Wrong password.']
+            }
+          }
+        end
+      else
         format.js {
           render template: 'shared/ujs/form_errors.js.erb',
           locals: {
             ids: ['email'],
             full_messages: ['No user with that email exists.']
-          }
-        }
-      elsif !user.authenticate params[:session][:password]
-        format.js {
-          render template: 'shared/ujs/form_errors.js.erb',
-          locals: {
-            ids: ['email'],
-            full_messages: ['Wrong password.']
           }
         }
       end
