11b151a43e6eee3683c7adea53bfd376e49dc116
Dan Mundy <drm.mundy@gmail.com>
2012-07-31 21:06:20 -0400

    Adds a link for users to delete their accounts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
-  before_filter :correct_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find params[:id]
@@ -58,6 +58,10 @@ class UsersController < ApplicationController
         }
       end
     end
+  end
+
+  def destroy
+    @user.destroy
   end
 
   private
